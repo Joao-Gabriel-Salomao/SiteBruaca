92be4c011ebf11a4dc999beb7c09555f678a457f
João Gabriel <j2005gabriel@gmail.com>
2021-09-05 16:33:09 -0400

    o

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import express from "express";
-
+import path from 'path';
 const app = express();
+
+/**
+ * configurações das paginas
+ */
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('views engine', 'pug' )
+
+/*
+*Criando a rota da página inicial */
+app.get('/', (req, res) => res.render('index'));
+
 
 export default app;
